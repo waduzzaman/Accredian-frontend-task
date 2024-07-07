5bb19ae07fcf264b52da4ceb26505d770af4a949
Waduzzaman <waduzzaman@gmail.com>
2024-07-06 23:00:52 -0400

    image path updated

--- src/pages/Home/Home/HowDoRefer.jsx
+++ src/pages/Home/Home/HowDoRefer.jsx
@@ -5,7 +5,7 @@ const HowDoRefer = () => {
         <div className="flex flex-col items-center relative bg-blue-50 py-10">
             <h3 className="text-2xl font-semibold mb-4 my-2">How Do <span className="text-blue-500">Refer?</span></h3>
             <div className="w-2/3 bg-blue-100">
-            <img className="w-full" src="/src/assets/design.png" alt="Design" />
+            <img className="w-full" src="./design.png" alt="Design" />
         </div>
             <button className="absolute bottom-10 px-12 text-white py-1 bg-blue-500 rounded-md bg-blue-00">
                 Refer Now
